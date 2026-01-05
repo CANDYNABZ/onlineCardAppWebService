3bd088ea084c154df770516de50ebfe479a7b03e
CANDYNABZ <nix9644@gmail.com>
2026-01-05 13:28:13 +0800

    express app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const mysql = require('mysql2/promise');
+require ('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    host: process.env.DB_USER,
+    password : process.env. DB_PASSWORD,
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT,
+    waitForConnections : true ,
+    connectionLimit : 100,
+    queueLimit : 0,
+
+};
+const app = express();
+app.use(express.json());
+
+//start the server 
+app.listen(port,()=> {
+    console.log('server running on port',port);
+});
+ //example route :get all card
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allcards' });
+    }
+});
